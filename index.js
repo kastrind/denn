aa2d9b23d314ebf98739b7063359b92b8b1511fb
Dimitris <kastrind@gmail.com>
2020-06-08 10:07:30 +0300

    WIP on iris

--- index.js
+++ index.js
@@ -309,8 +309,8 @@ let y = [
 let formation = [{"neurons": 5, "dropout": 0.0}];
 let learning_rate = 0.01;
 
-var nn = new NeuralNetwork(X, y, formation, learning_rate, Activation.relu);
-nn.train(800, 3, true);
+var nn = new NeuralNetwork(X, y, formation, learning_rate, Activation.sigmoid);
+nn.train(800, 10, true);
 //console.log(nn.predict([[6.2,3.4,5.4,2.3]]));
 let serialization_path = './nn-model.json';
 nn.serialize(serialization_path);
