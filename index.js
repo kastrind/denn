1f55d670f29a28d172ac6b36e49df4a57ba98622
Dimitris <kastrind@gmail.com>
2020-06-09 02:57:56 +0300

    Tidied up index

--- index.js
+++ index.js
@@ -9,20 +9,15 @@ dataset = DataSet.shuffle(dataset);
 let datasetXY = DataSet.separateXY(dataset, 4, true);
 let train_test = DataSet.separateTrainTest(datasetXY, 0.2);
 
-console.log(train_test.train.X.length+ " "+train_test.train.Y.length +" "+train_test.train.Y_one_hot.length);
-console.log(train_test.test.X.length+ " "+train_test.test.Y.length +" "+train_test.test.Y_one_hot.length);
-
 let X = train_test.train.X;
 let Y = train_test.train.Y_one_hot;
 let formation = [{"neurons": 6, "dropout": 0.0}];
 let learning_rate = 0.05;
-
 var nn = new NeuralNetwork(X, Y, formation, learning_rate, Activation.sigmoid);
-nn.train(1000, 3, true);
-//console.log(nn.predict([[6.2,3.4,5.4,2.3]]));
+nn.train(100, 3, true);
+
 let serialization_path = './nn-model.json';
 nn.serialize(serialization_path);
-
 var nn2 = NeuralNetwork.deserialize(serialization_path);
 //nn2.train(100, 20, true);
 
